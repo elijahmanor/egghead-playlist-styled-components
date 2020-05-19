59acfb361a4b86d11ff54b6d2b08790a2941d08c
Elijah Manor <elijah.manor@gmail.com>
2020-05-18 23:56:57 -0500

    Commit the CSS version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from 'react'
+import faker from 'faker'
+import Profile from './Profile'
+import './styles.css'
+
+const user = faker.helpers.userCard()
+user.aboutMe = faker.hacker.phrase()
+user.avatar = faker.image.avatar()
+user.backgroundUrl = `http://placeimg.com/640/480/tech?t=${Date.now()}`
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Profile user={user} />
     </div>
-  );
+  )
 }
